19555bbb205363bfaea4b4d658e01dfaeccb21b7
Thanhpt_relipa <thanhpt@relipasoft.com>
2022-11-24 16:51:16 +0700

    Fix-bug date add member(admin)

--- front_end/src/pages/admin/register/registerSuccess.js
+++ front_end/src/pages/admin/register/registerSuccess.js
@@ -16,7 +16,9 @@ const RegisterSuccess = () => {
       title="You have successfully registered an account"
       subTitle={`Email: ${accountUser.email} Password: ${accountUser.password}, password needs to be changed when created`}
       extra={[
-        <Button onClick={() => history.push("/register")}>Finished</Button>,
+        <Button onClick={() => history.push("/register-users")}>
+          Finished
+        </Button>,
       ]}
     />
   );
